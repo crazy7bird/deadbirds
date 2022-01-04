13301093c910f9f1f102f9b900ab35a72f6c1c92
CrazyBird <roy7alexandre@gmail.com>
2022-01-04 10:28:09 +0100

    Initial commit
    Initial commit files

---
+++ deadbirds/script.js
@@ -0,0 +1,120 @@
+var gl_initialCRO = 0;
+var gl_AP = 0;
+var gl_validcom = 0;
+var gl_netFee = 0;
+
+const hoursInYear = 8760;
+
+function gain_doing_nothing(){
+
+  var gain =  gl_initialCRO * gl_AP / 100;
+  var validatorGain = gain*gl_validcom/100;
+
+  gain = gain - validatorGain - gl_netFee;
+  //gain += gl_initialCRO
+
+  document.getElementById("doing_nothing").innerHTML = "By doing only one annual clain you will gain <imp>"+ gain.toFixed(2) +"</imp> CRO, your validator will earn "+validatorGain.toFixed(2)+" CRO"
+}
+
+function gain_restack_every_hours(hours){
+
+  var number_of_restaks = hoursInYear / hours;
+  var gain = 1 + ((gl_AP *hours) / (100 * hoursInYear));
+  gain = gain ** number_of_restaks;
+  gain = (gain * gl_initialCRO) - gl_initialCRO;
+  var validatorGain = gain  *gl_validcom/100;
+  gain = gain - validatorGain - (gl_netFee * number_of_restaks);
+  return {gain,validatorGain};
+}
+
+function optimum_restack_hours_seeker(){
+  var hours = 2;
+  var lastgain = gain_restack_every_hours(1);
+  for(;hours <= hoursInYear;hours++)
+  {
+    var {gain,validatorGain} = gain_restack_every_hours(hours);
+    if( lastgain >= gain)
+    {
+      hours--;
+      break;
+    }
+    lastgain = gain;
+  }
+  document.getElementById("doing_periodical").innerHTML = "By restaking every <imp>"+ hours +"</imp> H, you will gain <imp>"+lastgain.toFixed(2)+" </imp>CRO, and your validator will earn "+validatorGain.toFixed(2)+" CRO."
+}
+
+function gain_restack_every_earn(earnTrig){
+
+  var oneHourPcent = (gl_AP / (hoursInYear * 100));
+  var totalGain = 0;
+  var validatorPart = 1 - (gl_validcom /100);
+
+  for(let hour = 0; hour <hoursInYear;)
+  {
+    let oneHourGain = (gl_initialCRO + totalGain) * oneHourPcent;
+    let hoursForTrig = earnTrig / oneHourGain;  
+    hour += hoursForTrig;
+    if ( hour <= hoursInYear )
+    {
+      totalGain += (oneHourGain * hoursForTrig * validatorPart) - gl_netFee;
+    }
+    else //l’heure dépasse un peu 
+    {
+      let remainStack = oneHourGain * (hoursForTrig - (hour - hoursInYear)) * validatorPart;
+      if(remainStack > gl_netFee)
+      {
+        totalGain += remainStack - gl_netFee;
+      }
+    }
+  }
+  return totalGain;
+}
+
+function optimum_restack_earn_seeker(){
+  var earnTrig = 0.1;
+  var lastgain = gain_restack_every_earn(0.01);
+  for(;earnTrig <= 1;earnTrig+=0.01)
+  {
+   
+    var gain = gain_restack_every_earn(earnTrig);
+    
+    //console.log("( "+ earnTrig.toFixed(2) +" )  : "+ gain.toFixed(2));
+    if( lastgain >= gain)
+    {
+      earnTrig-=0.01
+      break;
+    }
+    lastgain = gain;
+    
+  }
+
+    document.getElementById("doing_triggerCRO").innerHTML = "By restaking every <imp>"+ earnTrig.toFixed(2) +"</imp> CRO, you will earn <imp>"+ lastgain.toFixed(2) +"</imp> CRO"
+  
+}
+
+setInterval(function() {
+
+  var l_initialCRO =  document.getElementById("initialCRO").value;
+  var l_AP =  document.getElementById("AP").value;
+  var l_validcom =  document.getElementById("validcom").value;
+  var l_netFee =  document.getElementById("netFee").value;
+
+  if((l_initialCRO != gl_initialCRO) ||(l_AP != gl_AP) || (l_validcom != gl_validcom) || (l_netFee != gl_netFee) )
+  {
+    //Parsefloat to be sure having a number
+    gl_initialCRO = parseFloat(l_initialCRO);
+    gl_AP = parseFloat(l_AP);
+    gl_validcom = parseFloat(l_validcom);
+    gl_netFee = parseFloat(l_netFee);
+
+    gain_doing_nothing();
+    optimum_restack_hours_seeker();
+
+    optimum_restack_earn_seeker();
+  }
+
+
+}, 1500);
+
+
+
